9e1dc505409eb591f389c69f8f43612c8308f699
Raj <raaz2507@gmail.com>
2025-08-03 03:11:38 +0530

    set static path in setting

--- activityTracker/settings.py
+++ activityTracker/settings.py
@@ -114,9 +114,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS =  [os.path.join(BASE_DIR, 'static')] #development mode
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #for collectstatic
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
